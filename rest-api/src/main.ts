f18a691763bbbd5ff15ca603a686cd51ccbc9764
blame Nikola Slavchev <nslavchev48@gmail.com>
2023-04-05 11:47:21 +0300

    cookie based session authentication implementation

--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.use(
+    cookieSession({
+      keys: ['asdfasfd'],
+      maxAge: 15 * 60 * 1000, // 15 min
+    }),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- rest-api/src/users/users.controller.ts
+++ rest-api/src/users/users.controller.ts
@@ -1,7 +1,48 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Session,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+
+import { RegisterUserDTO } from './dtos/register-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDTO } from './dtos/user.dto';
+import { AuthService } from './auth.service';
+import { LoginUserDTO } from './dtos/login-user.dto';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { User } from './user.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
+@Serialize(UserDTO)
 export class UsersController {
+  constructor(private authService: AuthService) {}
+
+  @Get('/whoami')
+  @UseGuards(AuthGuard)
+  whoAmI(@CurrentUser() user: User) {
+    return user;
+  }
+
   @Post('/signup')
-  createUser() {}
+  async registerUser(@Body() body: RegisterUserDTO, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signin')
+  async loginUser(@Body() body: LoginUserDTO, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Get('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
 }
